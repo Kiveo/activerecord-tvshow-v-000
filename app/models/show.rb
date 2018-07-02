d66b00113a9a7f4f568ced96ecb86b2d6bcc77d4
Maverick Tesini <mavericktesini@gmail.com>
2018-07-02 07:40:13 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -26,11 +26,11 @@ class Show < ActiveRecord::Base
     Show.sum(:rating)
   end
 
-  def popular_shows
+  def self.popular_shows
     Show.where("rating > 5")
   end
 
-  def shows_by_alphabetical_order
+  def self.shows_by_alphabetical_order
     Show.order(:name)
   end
 end
