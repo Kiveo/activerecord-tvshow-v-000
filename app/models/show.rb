cd46d85dd8c35af1b8ff5f0a703841c0ffcd8a3c
Maverick Tesini <mavericktesini@gmail.com>
2018-07-02 07:41:24 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -19,7 +19,7 @@ class Show < ActiveRecord::Base
   end
 
   def self.least_popular_show
-    Show.where("rating = ?", lowest_rating)[0]
+    Show.where("rating = ?", lowest_rating).first
   end
 
   def self.ratings_sum
